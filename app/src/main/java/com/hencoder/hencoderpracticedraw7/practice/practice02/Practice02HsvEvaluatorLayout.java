4a00305a93f554d111418d4fd44ecd8aad9b0c78
YuanGang <552185713@qq.com>
2018-09-26 15:16:57 +0800

    更新代码

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice02/Practice02HsvEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice02/Practice02HsvEvaluatorLayout.java
@@ -3,7 +3,9 @@ package com.hencoder.hencoderpracticedraw7.practice.practice02;
 import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
@@ -38,7 +40,8 @@ public class Practice02HsvEvaluatorLayout extends RelativeLayout {
             @Override
             public void onClick(View v) {
                 ObjectAnimator animator = ObjectAnimator.ofInt(view, "color", 0xffff0000, 0xff00ff00);
-                animator.setEvaluator(new HsvEvaluator()); // 使用自定义的 HsvEvaluator
+                // 使用自定义的 HsvEvaluator
+                animator.setEvaluator(new HsvEvaluator());
                 animator.setInterpolator(new LinearInterpolator());
                 animator.setDuration(2000);
                 animator.start();
@@ -47,11 +50,31 @@ public class Practice02HsvEvaluatorLayout extends RelativeLayout {
     }
 
     private class HsvEvaluator implements TypeEvaluator<Integer> {
+        float[] startHsv = new float[3];
+        float[] endHsv = new float[3];
+        float[] outHsv = new float[3];
 
-        // 重写 evaluate() 方法，让颜色按照 HSV 来变化
+        /**
+         * 重写 evaluate() 方法，让颜色按照 HSV 来变化
+         *
+         * @param fraction   动画完成度
+         * @param startValue 起始颜色
+         * @param endValue   结束颜色
+         * @return
+         */
         @Override
         public Integer evaluate(float fraction, Integer startValue, Integer endValue) {
-            return startValue;
+            Log.d("YGG", "evaluate fraction:" + fraction + "  startValue " + startValue + " endValue  " + endValue);
+            //把ARGB转换成HSV
+            Color.colorToHSV(startValue, startHsv);
+            Color.colorToHSV(endValue, endHsv);
+
+            //计算当前动画完成度所对应的颜色和
+            outHsv[0] = startHsv[0] + (endHsv[0] - startHsv[0]) * fraction;
+            outHsv[1] = startHsv[1] + (endHsv[1] - startHsv[1]) * fraction;
+            outHsv[2] = startHsv[2] + (endHsv[2] - startHsv[2]) * fraction;
+
+            return Color.HSVToColor(outHsv);
         }
     }
 }
